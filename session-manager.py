4bb9a1b3944e5d3330145de5489815a77689e2ee
Nick Le Large <nick.lelarge@kit.edu>
2024-06-19 14:58:14 +0200

    More refactoring, support workspaces

--- session-manager.py
+++ session-manager.py
@@ -37,16 +37,6 @@ def is_normal_window(window_id):
     return " _NET_WM_WINDOW_TYPE_NORMAL" in w_type
 
 
-def get_screen_resolution():
-    # get resolution and the workspace correction (vector)
-    xr_output = run_command("xrandr").split()
-    pos = xr_output.index("current")
-    resolution = [int(xr_output[pos + 1]), int(xr_output[pos + 3].replace(",", ""))]
-    vp_data = run_command("wmctrl -d").split()
-    curr_vp_data = [int(n) for n in vp_data[5].split(",")]
-    return [resolution, curr_vp_data]
-
-
 def get_display_layout_hash():
     xrandr_output = run_command("xrandr --listmonitors")
     return hashlib.sha256(xrandr_output.encode("utf-8")).hexdigest()
@@ -63,79 +53,89 @@ def get_app_name(pid):
 
 def save_session():
     window_list = [line.split() for line in run_command("wmctrl -lpG").splitlines()]
-    relevant_windows = [
-        [window[2], [int(n) for n in window[3:7]]]
-        for window in window_list
-        if is_normal_window(window[0]) == True
-    ]
-    for idx, window in enumerate(relevant_windows):
-        relevant_windows[idx] = (
-            f"{get_app_name(window[0])} {' '.join(map(str, window[1]))}"
-        )
+
     session_file = get_session_filepath()
     with open(session_file, "wt") as file:
-        for line in relevant_windows:
-            file.write(line + "\n")
+        for window in window_list:
+            if not is_normal_window(window[0]):
+                continue
+            workspace = window[1]
+            pid = window[2]
+            geometry = window[3:7]
+            app_name = get_app_name(pid)
+
+            file.write(
+                f"{app_name} {workspace} {geometry[0]} {geometry[1]} {geometry[2]} {geometry[3]}\n"
+            )
 
 
-def launch_app_window(app_name, x, y, width, height):
-    existing_windows = run_command("wmctrl -lp")
-    # fix command for certain apps that open in new tab by default
-    option = " --new-window" if app_name == "gedit" else ""
-
-    # fix command if process name and command to run are different
-    app_command = {
+def get_app_command(app_name):
+    app_name_to_command_map = {
         "gnome-terminal": "gnome-terminal",
         "chrome": "/usr/bin/google-chrome-stable",
         "mattermost-desk": "mattermost-desktop",
-    }.get(app_name, app_name)
+    }
+    return app_name_to_command_map.get(app_name, app_name)
 
-    subprocess.Popen(["/bin/bash", "-c", f"{app_command}{option}"])
-    # fix exception for Chrome (command = google-chrome-stable, but processname = chrome)
-    app_name = "chrome" if "chrome" in app_name else app_name
 
-    t = 0
-    while t < 30:
-        new_windows = [
-            w.split()[0:3]
-            for w in run_command("wmctrl -lp").splitlines()
-            if not w in existing_windows
-        ]
-        processes = [
-            [
-                (p, w[0])
-                for p in run_command("ps -e ww").splitlines()
-                if app_name in p and w[2] in p
-            ]
-            for w in new_windows
-        ]
-        if processes:
-            time.sleep(0.5)
-            window_id = processes[0][0][1]
-            commands = [
-                f"wmctrl -ir {window_id} -b remove,maximized_horz",
-                f"wmctrl -ir {window_id} -b remove,maximized_vert",
-                f"wmctrl -ir {window_id} -e 0,{x},{y},{width},{height}",
-            ]
-            for command in commands:
-                subprocess.call(["/bin/bash", "-c", command])
+def get_app_options(app_name):
+    app_options_map = {
+        "gedit": "--new-window",
+    }
+    return app_options_map.get(app_name, "")
+
+
+def set_window_geometry(window_id, workspace, x, y, width, height):
+    commands = [
+        f"wmctrl -ir {window_id} -b remove,maximized_horz",
+        f"wmctrl -ir {window_id} -b remove,maximized_vert",
+        f"wmctrl -ir {window_id} -t {workspace}",
+        f"wmctrl -ir {window_id} -e 0,{x},{y},{width},{height}",
+    ]
+    for command in commands:
+        run_command(command)
+
+
+def launch_app_window(app_name, workspace, x, y, width, height):
+    existing_windows = run_command("wmctrl -lp")
+
+    processes = run_command("ps -e -o pid,cmd").splitlines()
+    for process in processes:
+        if app_name in process:
+            return
+
+    app_command = get_app_command(app_name)
+    options = get_app_options(app_name)
+
+    subprocess.Popen(["/bin/bash", "-c", f"{app_command} {options}"])
+
+    try_count = 0
+    max_tries = 30
+    window_resized = False
+    while not window_resized and try_count < max_tries:
+        for windows in run_command("wmctrl -lp").splitlines():
+            window_id = windows.split()[0]
+            if window_id in existing_windows:
+                continue
+            set_window_geometry(window_id, workspace, x, y, width, height)
+            window_resized = True
             break
+
         time.sleep(0.5)
-        t = t + 1
 
 
 def restore_session():
-    screen_resolution_correction = get_screen_resolution()[1]
     session_file = get_session_filepath()
     try:
         lines = [line.split() for line in open(session_file).read().splitlines()]
         for line in lines:
             app_name = line[0]
-            x = str(int(line[1]) - screen_resolution_correction[0])
-            y = str(int(line[2]) - screen_resolution_correction[1] - 24)
-            width = line[3]
-            height = line[4]
-            launch_app_window(app_name, x, y, width, height)
+            workspace = line[1]
+            x = int(line[2])
+            y = int(line[3])
+            width = int(line[4])
+            height = int(line[5])
+            launch_app_window(app_name, workspace, x, y, width, height)
     except FileNotFoundError:
         print("No session file found for the current display layout.")
 
